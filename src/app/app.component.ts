dce2e2516dca8882f071fdb4a5c6b68dcb386546
Jesús <vixximpred@gmail.com>
2018-08-09 23:22:04 +0200

    Added capability to publish new articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,9 @@ export class AppComponent {
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement) {
-    console.log(`Adding article: ${title.value} (${link.value})`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 
